03681f9dc0d78183a968061acdef43e82da1a5ae
Tiago Kennedy <128265741+T-St0rm@users.noreply.github.com>
2023-07-29 17:26:08 -0400

    respondida

---
+++ 03/index.js
@@ -0,0 +1,7 @@
+var valor_total = 150
+var meu_dinheiro = 70
+var desconto = valor_total - meu_dinheiro
+console.log("seu desconto é de " + desconto + " reais")
+
+var percentual = desconto / valor_total * 100
+console.log("e o percentual de desconto é " + percentual + "%")
