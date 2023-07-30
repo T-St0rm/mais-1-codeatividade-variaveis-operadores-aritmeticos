799ccddf3f3492d44f03e0782b85cf8d11cc6077
Tiago Kennedy <128265741+T-St0rm@users.noreply.github.com>
2023-07-30 17:37:37 -0400

    respondida

--- 06/index.js
+++ 06/index.js
@@ -1,1 +1,8 @@
 const raio = 1
+const pi = 3.14
+
+var calc_comprimento = 2 * pi * raio
+var calc_area = pi * raio **2
+
+console.log("Comprimento " + calc_comprimento)
+console.log("area " + calc_area)
