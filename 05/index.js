7e427b73c448768512106bbeccc0f789ed730a5e
Tiago Kennedy <128265741+T-St0rm@users.noreply.github.com>
2023-07-30 17:07:12 -0400

    respondida

---
+++ 05/index.js
@@ -0,0 +1,7 @@
+var distancia = parseFloat("800 metros")
+var tempo = parseFloat("18 segundos")
+
+var velocidade = distancia / tempo
+
+var velocidade_km = velocidade * 3.6
+console.log("velocidade " + velocidade_km + "km/h")
