5ed4c9d2fe40f32f9189d923e7a18726c77bf1a2
Tiago Kennedy <128265741+T-St0rm@users.noreply.github.com>
2023-07-30 18:13:01 -0400

    respondida

---
+++ 08/index.js
@@ -0,0 +1,13 @@
+/*$$ ax^2 + bx + c = 0 $$
+
+A fórmula de $delta$ é obtida por:
+
+$$ delta = b^2 - 4ac $$*/
+
+var a = 50
+var b = 10
+var c = 5
+
+var delta = 10**2 - 4 * 50 * 5
+
+console.log("Delta: " + delta)
